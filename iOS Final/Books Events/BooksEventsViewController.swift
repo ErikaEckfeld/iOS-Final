9e2b2d5752f2e7034c9940d618c54d8cf0a59f99
Erika Eckfeld <erikazprincess@aol.com>
2019-12-03 00:21:54 -0600

    rename

---
+++ iOS Final/Books Events/BooksEventsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BooksEventsViewController.swift
+//  Books Events
+//
+//  Created by Erika Eckfeld on 12/3/19.
+//  Copyright © 2019 Erika Eckfeld. All rights reserved.
+//
+
+import UIKit
+
+class BooksEventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
